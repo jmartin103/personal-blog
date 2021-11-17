6d8c03d635ba07ffefebaac52e589fa12578de92
jmartin103 <jwmartin1222@gmail.com>
2021-11-17 10:54:56 -0500

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <>
+            <p>I'm a programmer</p>
+        </>
+    )
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <img src="images/photo.jpg" alt="" height="250" />
+            <h1 id="welcome">Hi, I'm John!</h1> 
+            <h3>Professonial Web and Application Developer</h3>
+        </>
+    )
+}
+
+export default Home;
